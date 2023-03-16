f6e867c0422a8baf8a00ab84ce18a46179b56c97
Ellie <ellie@omnistory.net>
2023-03-16 11:30:34 +0900

    [feat] model 설정하고 데이터 받아오기

--- lib/screens/api_service.dart
+++ lib/screens/api_service.dart
@@ -1,16 +1,26 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var w in webtoons) {
+        final toon = WebtoonModel.fromJason(w);
+        // print(toon.title);
+        webtoonInstances.add(toon);
+      }
+      print(webtoonInstances);
+      return webtoonInstances;
     }
     throw Error();
   }
