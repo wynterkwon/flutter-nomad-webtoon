076756692f07f1b1f14ce3fa413c8f0a474300fb
WinterKwon <winterkwon7017@gmail.com>
2023-03-20 00:01:39 +0900

    [feat] 상세페이지 이미지 영역 가져오기

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -21,6 +21,18 @@ class DetailedScreen extends StatelessWidget {
         foregroundColor: Colors.green,
         elevation: 2,
       ),
+      body: Container(
+        width: 250,
+        clipBehavior: Clip.hardEdge,
+        child: Image.network(thumb),
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.circular(10), boxShadow: [
+          BoxShadow(
+              blurRadius: 3,
+              offset: const Offset(10, 10),
+              color: Colors.black.withOpacity(0.5))
+        ]),
+      ),
     );
   }
 }
